6e1960983370965292a01f104bb605fb2b8b9f02
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-10-01 12:15:24 +0000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import {
+  BottomNavigation,
+  Hero,
+  Jambotron,
+  NavBar,
+  Preview,
+  // TimeLines,
+} from "./components";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <div className="bg-white dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-lg text-center lg:py-16">
+          <p className="mb-3 text-gray-500 dark:text-gray-400 first-line:uppercase first-line:tracking-widest first-letter:text-7xl first-letter:font-bold first-letter:text-gray-900 dark:first-letter:text-gray-100 first-letter:mr-3 first-letter:float-left">
+            Track work across the enterprise through an open, collaborative
+            platform. Link issues across Jira and ingest data from other
+            software development tools, so your IT support and operations teams
+            have richer contextual information to rapidly respond to requests,
+            incidents, and changes.
+          </p>
+          <p className="text-gray-500 dark:text-gray-400">
+            Deliver great service experiences fast - without the complexity of
+            traditional ITSM solutions.Accelerate critical development work,
+            eliminate toil, and deploy changes with ease, with a complete audit
+            trail for every change.
+          </p>
+        </div>
+
+        <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+            <img
+              className="rounded-t-lg"
+              src="Photo by Mikhail Nilov from Pexels: https://www.pexels.com/photo/employees-standing-in-white-long-sleeve-shirts-holding-and-reading-paperwork-inside-an-office-8297449/"
+              alt=""
+            />
+          </a>
+          <div className="p-5">
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Noteworthy technology acquisitions 2021
+              </h5>
+            </a>
+            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+              Here are the biggest enterprise technology acquisitions of 2021 so
+              far, in reverse chronological order.
+            </p>
+            <a
+              href="#"
+              className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                className="w-3.5 h-3.5 ml-2"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 14 10"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                />
+              </svg>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <Jambotron />
+      <Preview />
+      {/* <TimeLines /> */}
+      <BottomNavigation />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+import NavBar from "./NavBar";
+import BottomNavigation from "./BottomNavigation";
+import Jambotron from "./Jambotron";
+import Hero from "./Hero";
+import TimeLines from "./TimeLines";
+import Preview from "./Preview";
+
+export { BottomNavigation, NavBar, Hero, Jambotron, TimeLines, Preview };
