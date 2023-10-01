257544118d9bd9e8c99f12b1524f6d7047d1c4fa
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-10-01 14:01:34 +0000

    changes

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -48,7 +48,7 @@ const Services = () => {
       <div className="bg-white dark:bg-gray-900">
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-            <h2 className="text-2xl font-bold text-gray-900 dark:text-gray-200 text-center">
+            <h2 className="text-2xl md:text-4xl font-bold text-gray-900 dark:text-gray-200 text-center">
               Our Services
             </h2>
 
