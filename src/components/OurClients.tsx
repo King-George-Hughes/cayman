06545f971387e9fcadab0d3fd51a62b7ec3d3e5b
King-George-Hughes <hughesgeorgeme@gmail.com>
2023-10-13 21:35:30 +0000

    changes

--- src/components/OurClients.tsx
+++ src/components/OurClients.tsx
@@ -8,56 +8,56 @@ export default function OurClients() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-7 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-8">
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="/clients/act.jpeg"
+            src="images/clients/act.jpeg"
             alt="Transistor"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-            src="/clients/dad.jpeg"
+            src="images/clients/dad.jpeg"
             alt="Tuple"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-10 w-full object-contain lg:col-span-1"
-            src="/clients/rijalco.jpeg"
+            src="images/clients/rijalco.jpeg"
             alt="Reform"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-            src="/clients/lotus.jpeg"
+            src="images/clients/lotus.jpeg"
             alt="SavvyCal"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="/clients/property.jpeg"
+            src="images/clients/property.jpeg"
             alt="Statamic"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="/clients/axis.jpeg"
+            src="images/clients/axis.jpeg"
             alt="Statamic"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="/clients/st jude.jpeg"
+            src="images/clients/st jude.jpeg"
             alt="Statamic"
             width={158}
             height={48}
           />
           <img
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-            src="/clients/zest.jpeg"
+            src="images/clients/zest.jpeg"
             alt="Statamic"
             width={158}
             height={48}
