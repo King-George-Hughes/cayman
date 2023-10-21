2f8cc655c86891e76e14532f227658688deeee39
King-George-Hughes <121091106+King-George-Hughes@users.noreply.github.com>
2023-10-21 17:28:54 +0000

    changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -38,7 +38,7 @@ export default function NavBar() {
         <button
           data-collapse-toggle="navbar-solid-bg"
           type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 z-30"
           aria-controls="navbar-solid-bg"
           aria-expanded="false"
           onClick={() => setShowMenu((prevState) => !prevState)}
@@ -84,32 +84,42 @@ export default function NavBar() {
         {/* Menu for mobile view */}
         <AnimatePresence>
           {showMenu && (
-            <motion.div
-              initial={{ y: "-60px", opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-              exit={{ x: "100%", opacity: 0 }}
-              className="absolute md:hidden w-full md:w-auto top-[55px] right-0 z-10"
-              id="navbar-solid-bg"
-            >
-              <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-                {routes.map((route) => {
-                  return (
-                    <li>
-                      <NavLink
-                        to={route.url}
-                        className={({ isActive }) =>
-                          isActive
-                            ? "block py-2 pl-3 pr-4 text-gray-800 bg-[#dacb46] rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-gray-800 md:dark:bg-transparent"
-                            : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
-                        }
-                      >
-                        {route.name}
-                      </NavLink>
-                    </li>
-                  );
-                })}
-              </ul>
-            </motion.div>
+            <>
+              <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                className="fixed md:hidden w-full h-full top-0 left-0 right-0 bottom-0 bg-gradient-to-br from-[rgba(0,0,0,0.5)] to-[rgba(0,0,0,0.6)] backdrop-blur-[2px]"
+                onClick={() => setShowMenu(false)}
+              />
+
+              <motion.div
+                initial={{ y: "-60px", opacity: 0, scale: 0 }}
+                animate={{ y: 0, opacity: 1, scale: 1 }}
+                exit={{ x: "100%", opacity: 0 }}
+                className="absolute md:hidden w-2/3 md:w-auto top-[70px] right-5 z-10 border-gray-200 border-b-2 bg-gray-100 dark:bg-gray-800 dark:border-gray-700"
+                id="navbar-solid-bg"
+              >
+                <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+                  {routes.map((route) => {
+                    return (
+                      <li>
+                        <NavLink
+                          to={route.url}
+                          className={({ isActive }) =>
+                            isActive
+                              ? "block py-2 pl-3 pr-4 text-gray-800 bg-[#dacb46] rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-gray-800 md:dark:bg-transparent"
+                              : "block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:p-0 dark:text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
+                          }
+                        >
+                          {route.name}
+                        </NavLink>
+                      </li>
+                    );
+                  })}
+                </ul>
+              </motion.div>
+            </>
           )}
         </AnimatePresence>
         {/* Menu for mobile view */}
